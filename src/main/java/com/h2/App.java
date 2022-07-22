ef1eabadf61cfa8bfead849b39e832dc400fc25b
Phakawat-Rangsadthong <p.rangsadthong@accenture.com>
2022-07-22 16:44:15 +0700

    Module 2

--- src/main/java/com/h2/App.java
+++ src/main/java/com/h2/App.java
@@ -8,10 +8,24 @@ public class App
 {
     public static void main( String[] args )
     {
+
         System.out.println( "Hello World!" );
     }
 
     public static int doubleTheNumber(int number) {
-        return -1;
+
+        return 2*number;
+
     }
+
+    public static int add(int[] numbers){
+        int sum = 0;
+        for (int n: numbers) {
+            sum += n ;
+        }
+        return sum;
+    }
+
+    
+
 }
